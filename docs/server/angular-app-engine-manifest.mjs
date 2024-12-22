9eb946d7849f60845c0f36eb8234fb0a7031888e
Vigneshwaran <hivigneshwarans@gmail.com>
2024-12-22 16:22:35 +0100

    Application code

--- docs/server/angular-app-engine-manifest.mjs
+++
@@ -1,7 +0,0 @@
-
-export default {
-  basePath: '/angular-simple-app/',
-  entryPoints: {
-    '': () => import('./main.server.mjs')
-  },
-};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  imports: [
+    NgForOf
+  ],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-simple-app';
+  title = 'AlphaCorporation';
+  cards = [
+    {
+      'name': 'Elements',
+      'icon': 'menu'
+    },
+    {
+      'name': 'Forms',
+      'icon': 'list_alt'
+    },
+    {
+      'name': 'Alerts, Frame & Windows',
+      'icon': 'tab'
+    },
+    {
+      'name': 'Widgets',
+      'icon': 'widgets'
+    },
+    {
+      'name': 'Interactions',
+      'icon': 'repeat_on'
+    },
+    {
+      'name': 'Book Store Application',
+      'icon': 'library_books'
+    }
+  ]
 }
