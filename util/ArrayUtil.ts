26886aee3035022c84a2d9b3d8ca608e4fdfca03
Vigneshwaran <hivigneshwarans@gmail.com>
2025-07-25 19:35:58 +0200

    Auto-generated commit

--- util/ArrayUtil.ts
+++ util/ArrayUtil.ts
@@ -1,9 +1,9 @@
-export const getRandomValue = <T extends unknown>(arr: T[]): T => {
+export const getRandomValue = <T>(arr: T[]): T => {
   const index = Math.floor(Math.random() * arr.length);
   return arr[index];
 };
 
-export const pickRandomItems = <T extends unknown>(arr: T[], n: number): T[] => {
+export const pickRandomItems = <T>(arr: T[], n: number): T[] => {
   const shuffled = Array.from(arr).sort(() => 0.5 - Math.random());
   return shuffled.slice(0, n);
 };
@@ -12,10 +12,10 @@ export const getNaturalNumbers = (count: number): number[] => {
   return Array.from({ length: count }, (_, i) => i + 1);
 };
 
-export const getSumOfArrayItems = (array: number[]) => {
+export const getSumOfArrayItems = (array: number[]): number => {
   return array.reduce((acc, num) => acc + num, 0);
 };
 
-export const getAverageOfArrayItems = (array: number[]) => {
-  return getSumOfArrayItems(array) / array.length;
+export const getAverageOfArrayItems = (array: number[], toFixed: number = 2): number => {
+  return Number((getSumOfArrayItems(array) / array.length).toFixed(toFixed));
 };
